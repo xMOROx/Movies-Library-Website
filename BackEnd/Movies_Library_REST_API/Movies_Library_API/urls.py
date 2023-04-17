2f7d6e349f5729c55c7dd7c528ac29b2b066ab76
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2023-04-17 13:47:55 +0200

    Add new endpoints for movies

--- BackEnd/Movies_Library_REST_API/Movies_Library_API/urls.py
+++ BackEnd/Movies_Library_REST_API/Movies_Library_API/urls.py
@@ -14,6 +14,11 @@ urlpatterns = [
         name="details of movie for user",
     ),
     re_path(
+        r"^users/(?P<user_id>[0-9]+)/movies/(?P<movie_id>[0-9]+)$",
+        user_movies_views.AddMovieToUserView.as_view(),
+        name="add movie to user",
+    ),
+    re_path(
         r"^admin/users$", auth_views.AdminUserListView.as_view(), name="admin users"
     ),
     re_path(
@@ -29,14 +34,33 @@ urlpatterns = [
     re_path(
         r"^movies/(?P<movie_id>[0-9]+)$",
         movies_views.movie_details_api,
-        name="movie_details_api",
+        name="movie details api",
+    ),
+    re_path(r"^movies/popular$", movies_views.popular_movies, name="popular movies"),
+    re_path(r"^movies/upcoming$", movies_views.upcoming_movies, name="upcoming movies"),
+    re_path(r"^movies/latest$", movies_views.latest_movies, name="latest movies"),
+    re_path(r"^movies/trending", movies_views.trending_movies, name="trending movies"),
+    re_path(
+        r"^movies/now_playing", movies_views.now_playing_movies, name="now playing"
     ),
     re_path(
-        r"^users/(?P<user_id>[0-9]+)/movies/(?P<movie_id>[0-9]+)$",
-        user_movies_views.AddMovieToUserView.as_view(),
-        name="add movie to user",
+        r"^movies/(?P<movie_id>[0-9]+)/credits$",
+        movies_views.movie_credits,
+        name="movie credits",
     ),
-    re_path(r"^popular_movies$", movies_views.popular_movies, name="popular_movies"),
-    re_path(r"^upcoming_movies$", movies_views.upcoming_movies, name="upcoming_movies"),
-    re_path(r"^latest_movies$", movies_views.latest_movies, name="latest_movies"),
+    re_path(
+        r"^movies/(?P<movie_id>[0-9]+)/recommendations$",
+        movies_views.movie_recommendations,
+        name="recommendations after movie",
+    ),
+    re_path(
+        r"^movies/(?P<movie_id>[0-9]+)/similar$",
+        movies_views.similar_movies,
+        name="similar movies",
+    ),
+    re_path(
+        r"^movies/(?P<movie_id>[0-9]+)/providers$",
+        movies_views.movie_provider,
+        name="movie providers",
+    ),
 ]
